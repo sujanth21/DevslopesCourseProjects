f11e78ceb6579c9ba9cf621564a36279ee612950
Sujanth <sujanth1987@gmail.com>
2017-11-22 19:55:06 +1100

    Create Goal VC UI added

---
+++ GoalPost/GoalPost/Controller/CreateGoalVC.swift
@@ -0,0 +1,39 @@
+//
+//  CreateGoalVC.swift
+//  GoalPost
+//
+//  Created by Sujanth Sebamalaithasan on 22/11/17.
+//  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+    
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var nextBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func backBtnPressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func shortTermBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func nextBtnPressed(_ sender: Any) {
+    }
+    
+}
---
+++ GoalPost/GoalPost/Enums/GoalType.swift
@@ -0,0 +1,15 @@
+//
+//  GoalType.swift
+//  GoalPost
+//
+//  Created by Sujanth Sebamalaithasan on 21/11/17.
+//  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    
+    case shortTerm = "Short Term"
+    case longTerm = "Long Term"
+}
