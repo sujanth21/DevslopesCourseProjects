88dbfdaad10ee09eb7f0eadcf03298050e815274
Sujanth <sujanth1987@gmail.com>
2017-12-08 09:30:58 +1100

    Alamofire added

--- PokeDex/PokeDex/Controller/PokemonDetailVC.swift
+++ PokeDex/PokeDex/Controller/PokemonDetailVC.swift
@@ -12,6 +12,19 @@ class PokemonDetailVC: UIViewController {
 
     var pokemon: Pokemon!
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokeIdLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,5 +33,9 @@ class PokemonDetailVC: UIViewController {
     
     }
 
-
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
